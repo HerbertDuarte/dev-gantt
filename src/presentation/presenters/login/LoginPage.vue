b92c0774789f73e9c595919935d96f93e1d26e0a
herberttivic <herbert@tivic.com.br>
2024-12-20 15:05:20 -0300

    feat: nova tela de login

--- src/presentation/presenters/login/LoginPage.vue
+++ src/presentation/presenters/login/LoginPage.vue
@@ -17,8 +17,28 @@ watch(isAuthenticated, (value) => {
 </script>
 
 <template>
-    <main class="flex flex-col items-center justify-center h-dvh">
-            <img class="size-36" src="images/logo.png" alt="Logo" />
-            <FormLogin />
+    <main class="flex items-center justify-center h-dvh bg-slate-100">
+        <div class="flex items-stretch justify-center w-full max-w-[1000px] bg-white rounded-lg shadow-md overflow-hidden">
+            <div class="bg-slate-200 gap-6 p-4 flex-1 flex flex-col items-center justify-center">
+                <div class="w-full text-xs">
+                    <p class="text italic px-2">Organize suas demandas e projetos da melhor forma.</p>
+                    <p class="text italic px-2">Acompanhe a performance da sua equipe.</p>
+                    <p class="text italic px-2">Reuna as documentações e refências dos seus projetos.</p>
+                    <p class="text italic px-2">Tudo isso em um só lugar.</p>
+                </div>
+                <img class="flex-1" src="images/illustration-1.svg" alt="Logo" />
+            </div>
+            <div class="flex flex-1 max-w-[400px] flex-col items-start justify-center p-5">
+                <div class="flex items-center justify-center gap-2 pb-6">
+                    <img class="size-5" src="images/logo.png" alt="Logo" />
+                    <span class="text-md font-medium text-slate-600">Ganttly</span>
+                </div>
+                <div class=" pb-4">
+                    <h1 class="text-2xl font-semibold text-slate-700 text-left">Seja bem vindo!</h1>
+                    <p class="text-xs">Sua experiência é nossa prioridade!</p>
+                </div>
+                <FormLogin />
+            </div>
+        </div>
     </main>
 </template>
--- src/presentation/presenters/login/components/FormLogin.vue
+++ src/presentation/presenters/login/components/FormLogin.vue
@@ -1,17 +1,29 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
 import { useFormLogin } from '../../../../infrastructure/store/form-login';
-
+import { RouterLink } from 'vue-router';
 const form = useFormLogin();
 const { password, username } = storeToRefs(form);
 </script>
 
 <template>
-    <form @submit.prevent="form.submit()" class="flex flex-col max-w-[500px] w-full justify-start gap-4 p-4">
-        <q-input class="inline flex-1 w-full" dense outlined placeholder="Digite seu login" v-model="username"
+    <form @submit.prevent="form.submit()" class="flex flex-col w-full justify-start gap-4 p-2">
+        <q-input class="inline flex-1 w-full" dense label="Username *" outlined v-model="username"
             type="text" required />
-        <q-input class="inline flex-1 w-full" placeholder="Digite sua senha" outlined dense v-model="password"
+        <q-input class="inline flex-1 w-full" label="Senha *" outlined dense v-model="password"
             type="password" required />
+        <div class="text-right">
+            <RouterLink class="hover:underline text-sky-700" to="">Esqueci minha senha</RouterLink>
+        </div>
         <q-btn type="submit" class="cti-button-dark">Entrar</q-btn>
+        <hr>
+        <q-btn type="submit" class="cti-button-ghost">
+            <span class="flex items-center gap-2">
+                <img class="size-5" src="images/google-icon.png" alt="google icon">
+                <span>Entrar com o Google</span>
+            </span>
+        </q-btn>
+
+        <p class="pt-2">Não possui conta? <RouterLink class="hover:underline text-sky-700" to="">Registre-se.</RouterLink></p>
     </form>
 </template>
